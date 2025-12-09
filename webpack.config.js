be0a79749effcc3a8d20f74e41ce33ae11c48a2a
wizardabhi77 <vemulapalliabhinav@gmail.com>
2025-12-09 23:24:43 +0530

    create basic setup for project start

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// webpack.config.js
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+        template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
